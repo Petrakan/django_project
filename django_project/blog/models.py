42ba3c125751f1f4df41587944f529e7494fbddd
Alexey Petrakov <petrakan.code@gmail.com>
2019-12-25 14:05:47 +0300

    init

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    """Модель категорий"""
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    slug = models.SlugField("Url", max_length=100)
