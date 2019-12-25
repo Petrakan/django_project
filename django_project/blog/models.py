3805528ef774fd084011ad76a5f23fe9ba8344eb
Alexey Petrakov <petrakan.code@gmail.com>
2019-12-25 19:26:12 +0300

    init

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -15,3 +15,84 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categoryes'
+
+
+
+class Tag(models.Model):
+    """Модель тегов"""
+    name = models.CharField(verbose_name="Имя", max_length=100)
+    slug = models.SlugField("Tag-Slug", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='Tag'
+        verbose_name_plural='Tags'
+
+
+
+class Post(models.Model):
+    """Модель постов"""
+    title = models.CharField(verbose_name='Заголовок', max_length=255)
+    mini_text = models.TextField
+    text = models.TextField
+    created_date = models.DateTimeField(auto_now=True)
+    slug = models.SlugField("Slug-Url", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Post"
+        verbose_name_plural="Posts"
+
+
+
+class Comment(models.Model):
+    """Модель комментариев"""
+    text = models.TextField
+    created_date = models.DateTimeField(auto_now=True)
+    moderation = models.BooleanField
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Comment"
+        verbose_name_plural="Comments"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
